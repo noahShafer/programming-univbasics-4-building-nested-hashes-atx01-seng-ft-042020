31578fbb5996be3006fd739572dce07a8da57cab
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-14 22:08:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,6 +25,9 @@ def monopoly_with_third_tier
   # complexity.
   hash = monopoly_with_second_tier
   hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  hash[:railroads][:rent_in_dollars][:one_piece_owned] = 50
+  hash[:railroads][:rent_in_dollars][:one_piece_owned] = 100
+  hash[:railroads][:rent_in_dollars][:one_piece_owned] = 200
   hash
 end
 
