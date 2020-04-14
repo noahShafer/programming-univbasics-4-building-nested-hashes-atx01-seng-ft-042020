381180b0c7003c350f88e6eefed7db9312438932
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-14 22:11:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,5 +40,7 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  hash = monopoly_with_third_tier
+  hash[:railroads][:reading_railroad][:mortgage_value] = 100
+  hash
 end
